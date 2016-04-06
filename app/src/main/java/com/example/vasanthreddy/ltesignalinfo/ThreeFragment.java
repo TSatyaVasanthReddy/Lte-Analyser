173c336bd4671624c51928cc934c3ca981f97486
Satya Vasanth Reddy <vasanthreddy@Satyas-MacBook-Pro.local>
2016-04-06 10:46:56 +0530

    error but showing graph

--- app/src/main/java/com/example/vasanthreddy/ltesignalinfo/ThreeFragment.java
+++ app/src/main/java/com/example/vasanthreddy/ltesignalinfo/ThreeFragment.java
@@ -4,16 +4,29 @@ package com.example.vasanthreddy.ltesignalinfo;
  * Created by vasanthreddy on 31/03/16.
  */
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Random;
 import com.example.vasanthreddy.ltesignalinfo.R;
 
 
 
 public class ThreeFragment extends Fragment{
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> series;
+    private int lastX = 0;
+
 
     public ThreeFragment() {
         // Required empty public constructor
@@ -22,12 +35,60 @@ public class ThreeFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+            }
+    @Override
+     public void onResume() {
+        super.onResume();
+        // we're going to simulate real time with thread that append data to the graph
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // we add 100 new entries
+                for (int i = 0; i < 100; i++) {
+                    getActivity().runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    // sleep to slow down the add of entries
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
     }
+
+    // add random data to graph
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 10);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        getActivity().setContentView(R.layout.gview);
+        // we get graph view instance
+        GraphView graph = (GraphView)getActivity().findViewById(R.id.graph);
+        // data
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        // customize a little bit viewport
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(10);
+        viewport.setScrollable(true);
+
         return inflater.inflate(R.layout.fragment_three, container, false);
     }
 
